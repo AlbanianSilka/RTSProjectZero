b7cfac6a110d8f2c8bff8cbba1fb8a89e3323e7c
AlbanianSilka <andrii.khavriuta@gmail.com>
2024-02-24 20:33:38 +0200

    Started working on the buildings system
    Made a new class for buildings and a new button for building. TODO:
    - update system so that cursor might copy the object of a building fully
    - update the system so it might undestand on which ground it can and cannot be placed

--- Assets/Scripts/Units/Footman.cs
+++ Assets/Scripts/Units/Footman.cs
@@ -6,12 +6,4 @@ public class Footman : UnitRTS
 {
     protected override float moveSpeed => 6f;
     internal override int selectionPriority => 1;
-
-    private void Awake()
-    {
-        foreach (GameObject spellButton in spellButtons)
-        {
-            Debug.Log("Spell button name is: " + spellButton.name);
-        }
-    }
 }
---
+++ Assets/Scripts/buildings_manager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buildings_manager : MonoBehaviour
+{
+    public GameObject buildingPrefab;
+
+    private bool isPlacingBuilding = false;
+
+    public void StartBuilding()
+    {
+        isPlacingBuilding = true;
+
+        SpriteRenderer buildingSpriteRenderer = buildingPrefab.GetComponent<SpriteRenderer>();
+        Texture2D buildingTexture = buildingSpriteRenderer.sprite.texture;
+        Cursor.SetCursor(buildingTexture, Vector2.zero, CursorMode.Auto);
+    }
+
+    private void Update()
+    {
+        if (isPlacingBuilding)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                PlaceBuilding();
+            }
+            else if (Input.GetMouseButtonDown(1))
+            {
+                CancelBuilding();
+            }
+        }
+    }
+
+    private void PlaceBuilding()
+    {
+        Vector3 buildPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        buildPosition.z = 0f;
+
+        GameObject newBuilding = Instantiate(buildingPrefab, buildPosition, Quaternion.identity);
+
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+        isPlacingBuilding = false;
+    }
+
+    private void CancelBuilding()
+    {
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+        isPlacingBuilding = false;
+    }
+}
