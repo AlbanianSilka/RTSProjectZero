dfa208e02750f5b0d6b23b9216a972c63296507c
AlbanianSilka <andrii.khavriuta@gmail.com>
2024-03-05 19:48:01 +0200

    Buildings update
    Added an ability to stop construction and continue it if it was stopped for some reason.

--- Assets/Scripts/Units/Peasant.cs
+++ Assets/Scripts/Units/Peasant.cs
@@ -6,20 +6,91 @@ public class Peasant : UnitRTS
 {
     protected override float moveSpeed => 9f;
 
+    private Coroutine buildCouroutine;
+    private bool isBuilding;
+    private RTS_controller rtsController;
+
+    private void Awake()
+    {
+        rtsController = FindObjectOfType<RTS_controller>();
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        isBuilding = false;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        if (Input.GetMouseButton(1))
+        {
+            List<UnitRTS> selectedUnits = rtsController.selectedUnitRTSList;
+            if (selectedUnits.Contains(this))
+            {
+                Vector3 clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                HandleRightClick(clickPosition);
+            }
+        }
+    }
+
     public void StartBuildingProcess(GameObject building)
     {
-        RTS_building buildingComponent = building.GetComponent<RTS_building>();
-        StartCoroutine(Build(buildingComponent));
+        if (buildCouroutine == null)
+        {
+            isBuilding = true;
+            RTS_building buildingComponent = building.GetComponent<RTS_building>();
+            buildCouroutine = StartCoroutine(Build(buildingComponent));
+        }
     }
 
     private IEnumerator Build(RTS_building building)
     {
-        while(building.health < building.maxHealth)
+        while (building.health < building.maxHealth)
         {
             building.health += Time.deltaTime;
             yield return null;
         }
 
         building.finished = true;
+        StopBuildingProcess();
+    }
+
+    private void StopBuildingProcess()
+    {
+        StopCoroutine(buildCouroutine);
+        isBuilding = false;
+        buildCouroutine = null;
+    }
+
+    private void HandleRightClick(Vector3 clickPosition)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(clickPosition, Vector2.zero);
+        if (hit.collider != null)
+        {
+            GameObject clickedObject = hit.collider.gameObject;
+            if (clickedObject.CompareTag("Building"))
+            {
+                StartCoroutine(constructionPath(clickedObject));
+            }
+        } else
+        {
+            if (isBuilding)
+            {
+                StopBuildingProcess();
+            }
+        }
+    }
+
+    private IEnumerator constructionPath(GameObject buildingObject)
+    {
+        while (!HasReachedDestination())
+        {
+            yield return null;
+        }
+
+        StartBuildingProcess(buildingObject);
     }
 }
