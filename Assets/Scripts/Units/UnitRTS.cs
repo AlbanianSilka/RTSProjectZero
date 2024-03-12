46e815d7b6723c300fb96e812bdf22218d9d22af
AlbanianSilka <andrii.khavriuta@gmail.com>
2024-03-12 22:27:27 +0200

    Combat system ultra-mega-super alpha version
    Added death and started rewriting movement mechanic, will try to play with rigidbodies, maybe something will work out with it.

--- Assets/Scripts/Units/UnitRTS.cs
+++ Assets/Scripts/Units/UnitRTS.cs
@@ -7,10 +7,11 @@ public class UnitRTS : MonoBehaviour
 {
     private Vector2 destination;
     private bool isAttacking;
+    private UnitRTS followTarget;
 
     protected virtual float moveSpeed => 5f; 
     protected virtual float maxHp => 10f;
-    protected virtual float health { get; set; } = 10f;
+    protected virtual float health { get; set; } = 100f;
     protected virtual float attackSpeed { get; set; } = 1f;
     protected RTS_controller rtsController;
     [SerializeField] string team;
@@ -32,6 +33,11 @@ public class UnitRTS : MonoBehaviour
 
     protected virtual void Update()
     {
+        if(followTarget != null)
+        {
+            destination = followTarget.transform.position;
+        }
+
         transform.position = Vector2.MoveTowards(transform.position, destination, moveSpeed * Time.deltaTime);
 
         if (Input.GetMouseButton(1))
@@ -43,6 +49,13 @@ public class UnitRTS : MonoBehaviour
                 HandleRightClick(clickPosition);
             }
         }
+    }
+
+    protected virtual void Die()
+    {
+        OnDeath?.Invoke(this);
+
+        Destroy(gameObject);
     }
 
     public void MoveTo(Vector2 targetPosition)
@@ -81,8 +94,10 @@ public class UnitRTS : MonoBehaviour
 
                 if(clickedUnit != null && clickedUnit.team != this.team)
                 {
-                    MoveTo(clickPosition);
-                    StartCoroutine(attackPath(clickedUnit));
+                    followUnit(clickedUnit);
+                    clickedUnit = this.followTarget;
+                    //MoveTo(clickPosition);
+                    //StartCoroutine(attackPath(clickedUnit));
                 }
             }
         }
@@ -91,6 +106,27 @@ public class UnitRTS : MonoBehaviour
             StopAllCoroutines();
             isAttacking = false;
         }
+    }
+
+    public void followUnit(UnitRTS leader)
+    {
+        Vector3 direction = (leader.transform.position - transform.position).normalized;
+        transform.position += direction * Time.deltaTime * moveSpeed;
+
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 1f);
+        foreach (Collider2D collider in colliders)
+        {
+            if (collider.gameObject != gameObject && collider.gameObject.GetComponent<UnitRTS>() != null)
+            {
+                Vector3 avoidDirection = (transform.position - collider.transform.position).normalized;
+                transform.position += avoidDirection * Time.deltaTime * moveSpeed;
+            }
+        }
+    }
+
+    public void setFollowTarget(UnitRTS newTarget)
+    {
+        followTarget = newTarget;
     }
 
     private IEnumerator attackPath(UnitRTS attackedUnit)
@@ -120,11 +156,12 @@ public class UnitRTS : MonoBehaviour
         isAttacking = false; 
     }
 
-    protected virtual void Die()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        OnDeath?.Invoke(this);
-
-        Destroy(gameObject);
+        if (collision.gameObject.CompareTag("Environment"))
+        {
+            destination = transform.position;
+            Debug.Log("You've hit a stone");
+        }
     }
-
 }
