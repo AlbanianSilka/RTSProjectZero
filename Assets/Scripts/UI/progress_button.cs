534898d804e65a9a0fd449daf1d05e5be80bbf39
AlbanianSilka <andrii.khavriuta@gmail.com>
2024-03-21 17:31:01 +0200

    Progress in making training system
    Finally finished with correct transferring of unit icons onto middle UI section, also can now remove them from the list of a training units. TODO:
    - need to fix bug when the first element in unitsQueue is cancelled and deleted (need to restart coroutine)
    - need to add some progress bars so user could've see when training would be finished

--- Assets/Scripts/UI/progress_button.cs
+++ Assets/Scripts/UI/progress_button.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class progress_button : MonoBehaviour
 {
+    protected RTS_controller rtsController;
+
     public int buttonIndex;
+
+    private void OnDisable()
+    {
+        Destroy(gameObject);
+    }
+
+    public void cancelTraining()
+    {
+        rtsController = FindObjectOfType<RTS_controller>();
+        rtsController.selectedBuilding.unitsQueue.RemoveAt(buttonIndex);
+        Destroy(gameObject);
+        UI_controller.handleMiddleSection(rtsController.selectedBuilding.unitsQueue, rtsController.progressButtonPrefab);
+    }
 }
--- Assets/Scripts/UI/spell_button.cs
+++ Assets/Scripts/UI/spell_button.cs
@@ -5,4 +5,9 @@ using UnityEngine;
 public class spell_button : MonoBehaviour
 {
     public int spellBoxIndex;
+
+    private void OnDisable()
+    {
+        Destroy(gameObject);
+    }
 }
